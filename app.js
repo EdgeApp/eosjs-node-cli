754d6699a04279feec9ddd24d6b6a217801b03cc
Paul Puey <paul@edge.app>
2018-09-23 00:21:05 -0700

    Clean up most queries and make app.js standalone

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const figlet = require('figlet')
 const clear = require('clear')
 const chalk = require('chalk')
-const eos = require('./eos')
+const eosjs = require('eosjs')
+const { ecc } = eosjs.modules
 
-clear()
-console.log(
-  chalk.yellow(figlet.textSync('EOS-nodejs', { horizontalLayout: 'full' }))
-)
-console.log(chalk.blue('made by Marcel Morales'))
-console.log(chalk.green('Using standard private key on test net by default. Change --config-- in ./eos.js'))
-console.log(chalk.green('Make sure to have docker running as explained at https://github.com/EOSIO/eosjs/tree/master/docker'))
-console.log(chalk.green('out comment line to enable functions in ./app.js and get --help'))
+// clear()
+// console.log(
+//   chalk.yellow(figlet.textSync('EOS-nodejs', { horizontalLayout: 'full' }))
+// )
+// console.log(chalk.blue('made by Marcel Morales'))
+// console.log(chalk.green('Using standard private key on test net by default. Change --config-- in ./eos.js'))
+// console.log(chalk.green('Make sure to have docker running as explained at https://github.com/EOSIO/eosjs/tree/master/docker'))
+// console.log(chalk.green('out comment line to enable functions in ./app.js and get --help'))
 
 // ----TEST VARIABLES ----
-const pubKeyTest = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV' // from privKeyTest, local test net
-const pubKeyTest2 = 'EOS7pMyqadiD7DE7uZEHuEejZu2Qa7kiMmNVHf35bJEtqyniy8vBG' // from 'itamnetwork2' on main net
+// const pubKeyTest = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV' // from privKeyTest, local test net
+// const pubKeyTest2 = 'EOS7pMyqadiD7DE7uZEHuEejZu2Qa7kiMmNVHf35bJEtqyniy8vBG' // from 'itamnetwork2' on main net
 // const pubKeyTest3 = 'EOS86rDVGVU5UJAeAvDvRNKGJEDMjxGWr9vJBtBzCUW7s6zK2Puqp'
 
-const privKeyTest = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+// const privKeyTest = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
 // const privKeyTest2 = '5HrZWBGf6ovYBqdDkoGBqzXCKRxyXdkEmke6LVufN3zK4q9Hctc'
-const exampleTrxMainNet = '87134edc78cf9d1d183e896cbd96c8a89144511b33bce91c82f99321d0d2673a'
-const trBlockHeight = 10251887
-const acc1 = 'inita'
-const acc2 = 'initb'
-const accBinance = 'binancecleos'
-const bytes = 8192
-const stake_net_quantity = '10.0000 SYS'
-const stake_cpu_quantity = '10.0000 SYS'
-const transfer = 0
-const quantityTest = '2.0000 SYS'
+// const exampleTrxMainNet = '87134edc78cf9d1d183e896cbd96c8a89144511b33bce91c82f99321d0d2673a'
+// const trBlockHeight = 10251887
+// const acc1 = 'inita'
+// const acc2 = 'initb'
+// const accBinance = 'binancecleos'
+// const bytes = 8192
+// const stake_net_quantity = '10.0000 SYS'
+// const stake_cpu_quantity = '10.0000 SYS'
+// const transfer = 0
+// const quantityTest = '2.0000 SYS'
 
 // ALL  UNCOMMENTED  FUNCTIONS WILL BE INVOKED WHEN RUNNING node app.js in terminal
 
@@ -104,3 +105,138 @@ const quantityTest = '2.0000 SYS'
 // ----OTHER----
 //  converts '1.3000 EOS' --> 1.3, see floatRegex in eosj.js
 // console.log('tofloat: ', eos.toFloat('1.03002000'))
+
+// ----MAIN NET----
+const config = {
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906', // main net
+  httpEndpoint: 'https://api.eosnewyork.io:443', // main net
+  expireInSeconds: 60
+}
+
+const edgytesty311PrivateKey = '5JVjaFdWPtAdeWQBSAvh31R5HMnq2Qj7fbHdroQiSjCHrk4KFCC'
+const eos = eosjs(config)
+
+const main = async () => {
+
+  // ///////////////////////////////////////////////////
+  // Derive public key
+  const publicKey = ecc.privateToPublic(edgytesty311PrivateKey)
+  console.log(`publicKey: ${publicKey}`)
+
+  // ///////////////////////////////////////////////////
+  // Query for account name
+  let accountName
+  const accounts = await new Promise((resolve, reject) => {
+    eos.getKeyAccounts(publicKey, (error, result) => {
+      if (error) reject(error)
+      resolve(result)
+    })
+  })
+  if (accounts.account_names && accounts.account_names.length > 0) {
+    accountName = accounts.account_names[0]
+  }
+  console.log(`accountName: ${accountName}`)
+
+  // ///////////////////////////////////////////////////
+  // Query for account balances
+  const results = await eos.getCurrencyBalance('eosio.token', accountName)
+  if (results && results.length > 0) {
+    for (const r of results) {
+      if (typeof r === 'string') {
+        const balanceArray = r.split(' ')
+        if (balanceArray.length === 2) {
+          const exchangeAmount = balanceArray[0]
+          const currencyCode = balanceArray[1]
+          console.log(`exchangeAmount: ${exchangeAmount}`)
+          console.log(`currencyCode: ${currencyCode}`)
+        }
+      }
+    }
+  }
+
+  // ///////////////////////////////////////////////////
+  // Query account outgoing transactions
+  const actions = await eos.getActions('binancecleos')
+  console.log(actions)
+
+  if (actions.actions && actions.actions.length > 0) {
+    for (const action of actions.actions) {
+      const txid = action.action_trace.trx_id
+      const name = action.action_trace.act.name
+      console.log('------------------------------------------------')
+      console.log(`Txid: ${txid}`)
+      console.log(`Action type: ${name}`)
+      if (name === 'transfer') {
+        const { from, to, memo, quantity } = action.action_trace.act.data
+        const split = quantity.split(' ')
+        const [ exchangeAmount, currencyCode ] = split
+
+        console.log(`From: ${from}`)
+        console.log(`To: ${to}`)
+        console.log(`Memo: ${memo}`)
+        console.log(`Amount: ${exchangeAmount}`)
+        console.log(`currencyCode: ${currencyCode}`)
+      }
+    }
+  }
+
+  // ///////////////////////////////////////////////////
+  // Create an unsigned transaction
+  const destinationAccount = 'edgytestey43'
+  const unsignedTx = await eos.transaction({
+    actions: [
+      {
+        account: 'eosio.token',
+        name: 'transfer',
+        authorization: [{
+          actor: accountName,
+          permission: 'active'
+        }],
+        data: {
+          from: accountName,
+          to: destinationAccount,
+          quantity: '1.5000 SYS',
+          memo: ''
+        }
+      }
+    ]
+  },
+  {
+    sign: false,
+    broadcast: false
+  })
+  console.log(unsignedTx)
+
+  // ///////////////////////////////////////////////////
+  // Create a signed transaction
+  const signedTx = await eos.transaction({
+    actions: [
+      {
+        account: 'eosio.token',
+        name: 'transfer',
+        authorization: [{
+          actor: accountName,
+          permission: 'active'
+        }],
+        data: {
+          from: accountName,
+          to: destinationAccount,
+          quantity: '1.5000 SYS',
+          memo: ''
+        }
+      }
+    ]
+  },
+  {
+    sign: true,
+    broadcast: false,
+    keyProvider: [edgytesty311PrivateKey]
+  })
+  console.log(signedTx)
+}
+
+try {
+  main()
+} catch (e) {
+  console.log(e)
+}
